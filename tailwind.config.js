cbaa14f67bcacf61fe39f83d6d2161116fb4d10b
Léa Pollet <lea.pollet@epitech.eu>
2023-02-22 20:13:27 +0100

    [+] config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/*.html"],
+  content: ["./**/*.{html,js}", "./public/*.html"],
   theme: {
     fontFamily: {
       codec: ["CODEC", "sans-serif"],
